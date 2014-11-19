cf38acfb32f57284c7ea9b7aa2e8a6b4c7fb33a4
lonle <loanle1@yahoo.com>
2014-11-18 21:29:17 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,15 @@
 ##---------------- Programming Assingment 2 ------------------------------------------------------------------
-# Example: Caching the Inverse of a Matrix
-# In this example we introduce the <<- operator which can be used to assign a value to an object in an environment that is different from the current environment. Below are two functions that are used to create a special object that stores a numeric vector and cache's its mean.
-# The first function, makeCacheMatrix creates a special "matrix", which is really a list containing a function to
-# set the value of the vector
-# get the value of the vector
-# set the value of the mean
-# get the value of the mean
+# Caching the Inverse of a Matrix
+# The <<- operator which can be used to assign a value to an object in an environment that is different from 
+# the current environment. Below are two functions that are used to create a special object that stores a 
+# numeric matrix and cache's its inverse.
+# The first function, makeCacheMatrix creates a "matrix", which contains a function to
+# set the value of the matrix
+# get the value of the matrix
+# set the value of the inverse
+# get the value of the inverse
+# The second function, cacheSolve, checks to see if the inverse is cached.  If it is cached then it will retrieve
+# the inverse from cache.  Otherwise, it will calculate the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -21,7 +25,10 @@ makeCacheMatrix <- function(x = matrix()) {
              getinverse = getinverse)
 }
 
-## The following function calculates the inverse of the special "matrix" created with the above function. However, it first checks to see if the inverse has already been calculated. If so, it gets the inverse from the cache and skips the computation. Otherwise, it calculates the inverse of the data and sets the value of the inverse in the cache via the solve function.
+## The following function calculates the inverse of the "matrix" created with the above function. However, 
+# it first checks to see if the inverse has already been calculated. If so, it gets the inverse from the cache 
+# and skips the computation. Otherwise, it calculates the inverse of the data and sets the value of the inverse 
+# in the cache via the solve function.
  
 cacheSolve <- function(x, ...) {
         m <- x$getinverse()
